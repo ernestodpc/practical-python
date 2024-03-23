164c49f62592907ee04487e5c3e04e26bef8c556
twosComplementM1Max <79068349+ernestodpc@users.noreply.github.com>
2024-03-22 18:39:31 -0600

    Adding excercise 1.12: A Mistery

---
+++ Work/a_mystery.py
@@ -0,0 +1,17 @@
+# Excercise 1.12: A Mistery
+#
+# int() and float() can be used to convert numbers.
+# For example:
+int(123)
+
+float("1.23")
+
+# With that in mind, can you explain this behaviour?
+
+bool("False")
+
+# Converting the string "False" to a boolean value produces
+# the boolean True because only the empyt string "" evaluates
+# to a False Boolean object
+
+bool("")
